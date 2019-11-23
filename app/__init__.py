f710dfa1d127ef059eb693403b78e96370a33b0f
gabrielaventura <gabrielaalejandraventura@gmail.com>
2019-11-23 08:04:45 -0300

    arreglo init

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,4 @@
 from flask import Flask
-<<<<<<< HEAD
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-app = Flask(__name__)
-app.config from object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-from app import routes, models
-=======
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
@@ -18,4 +8,3 @@ app.config.from_object(Config)
 db=SQLAlchemy(app)
 migrate = Migrate(app,db)
 from app import routes
->>>>>>> d385cad6a48699c62376d7136a0094bf3e5e88e4
--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    nombre_apellido = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    contraseña = db.Column(db.String(128), nullable=False)
+    lider = db.Column(db.Boolean, default=False)
+    puesto = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.nombre_apellido)    
