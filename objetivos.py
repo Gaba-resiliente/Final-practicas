34b243b2a0238b84a6b9f5fdc87c05e3880cf4cc
gabrielaventura <gabrielaalejandraventura@gmail.com>
2019-11-23 16:23:17 -0300

    actualizacion objetivos.py

--- objetivos.py
+++ objetivos.py
@@ -1,8 +1,8 @@
-from app import app
-from app import db
-from app.models import User
+from app import app, db
+from app.models import User, Objetivos
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Objetivos': Objetivos}
+
