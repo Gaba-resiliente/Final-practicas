15f0a0eb93485f02fa648f94e4ed5f1772f02388
gabrielaventura <gabrielaalejandraventura@gmail.com>
2019-11-22 23:52:44 -0300

    cambios base

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask
-
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
-
-app.config.from_object(Config)
-
-from app import routes
-
-
-
-
+app.config from object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app import routes, models
