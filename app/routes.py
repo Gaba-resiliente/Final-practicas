b63c61e09bd127fae33e04165336d79caa7e89e8
gabrielaventura <gabrielaalejandraventura@gmail.com>
2019-11-22 21:22:07 -0300

    login.html

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+
+from app.forms import LoginForm
+
+# ...
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
