050fc10e4a6afd1bffa3849f35c39f8e7bdced73
gabrielaventura <gabrielaalejandraventura@gmail.com>
2019-11-26 14:29:11 -0300

    agregado clase token y actualizacion de models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import base64
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 
 class User (UserMixin,db.Model):
@@ -12,6 +14,8 @@ class User (UserMixin,db.Model):
       lider = db.Column(db.Boolean, default=False)
       puesto = db.Column(db.String(80), nullable=False)
       objetivos = db.relationship('Objetivos', backref='User', lazy='dynamic')
+      token = db.Column(db.String(32), index=True, unique=True)
+      token_expiration = db.Column(db.DateTime)
 
       def __repr__(self):
           return '<User {}>'.format(self.nombre_apellido)   
@@ -21,9 +25,27 @@ class User (UserMixin,db.Model):
 
       def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+      def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
 
-@login.user_loader
-def load_user(id):
+      def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+      @staticmethod
+      def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+        
+      @login.user_loader
+      def load_user(id):
         return User.query.get(int(id))
 
 class Objetivos (db.Model):
